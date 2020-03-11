a5b3113bdd45e5ad233288ecd045c93abedbc399
Paresh Jaiswal <pareshjaiswal22@hotmail.com>
2020-03-11 23:09:19 +0530

    spring-security: User Custom login security

---
+++ securityapp/src/main/java/com/security/config/DemoSecurityConfig.java
@@ -0,0 +1,39 @@
+package com.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		UserBuilder builder = User.withDefaultPasswordEncoder();
+		auth.inMemoryAuthentication().withUser(builder.username("Jhon").password("jhon@123").roles("EMPLOYEE"))
+									 .withUser(builder.username("mary").password("mary@123").roles("MANAGER"))
+									 .withUser(builder.username("susan").password("susan@123").roles("ADMIN"));
+	
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.anyRequest()
+			.authenticated()
+			.and().formLogin()
+			.loginPage("/user-login")
+			.loginProcessingUrl("/user-login-form")
+			.permitAll();
+	}
+	
+	
+	
+	
+	
+}
